866b9e51c0ee05649087a732ee15e72b2d084106
2024125083 <choihyunjae1224@gmail.com>
2026-04-20 15:26:30 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+
+    
+    int a,b;
+    int i = 0;
+    
+    scanf("%d",&b);
+    
+    do{
+        scanf("%d",&a);
+        i++;
+        
+        if(a>b){
+            printf("%d>?\n",a);
+
+        }else if(a<b){
+            printf("%d>?\n",a);
+        
+        }else{
+            printf("%d==?\n",a,a);
+            printf("%d\n",i);
+        }
+        
+    }while(a!=b);
+    
+    
+    return 0;
+}
